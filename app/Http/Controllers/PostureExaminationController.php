1b13364d867038e5dd1a27d7da522f4defce8bfd
MartinVerniere <martincho7verniere@yahoo.com>
2025-08-03 18:45:02 -0300

    Create PostureExaminationController.php

---
+++ app/Http/Controllers/PostureExaminationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\PostureExamination;
+
+class PostureExaminationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $examinaciones_postura = PostureExamination::all();
+        return view('examinacionesPostura.index', compact('examinaciones_postura'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(){}
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $nueva_examinacion_postura_id = PostureExamination::añadirExaminacionPostura($request);
+        return $nueva_examinacion_postura_id;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id){}
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id){}
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id){}
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id){}
+}
