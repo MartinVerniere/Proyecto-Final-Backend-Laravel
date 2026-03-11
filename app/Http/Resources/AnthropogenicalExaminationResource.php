8ab5dda7fd69830e819309be69dc7501feacc9a5
MartinVerniere <mn.verniere@gmail.com>
2026-03-11 10:13:01 -0300

    Update AnthropogenicalExaminationResource.php

--- app/Http/Resources/AnthropogenicalExaminationResource.php
+++ app/Http/Resources/AnthropogenicalExaminationResource.php
@@ -24,6 +24,7 @@ class AnthropogenicalExaminationResource extends JsonResource
 			'Peso' => $this->peso_paciente,
 			'Presion_arterial' => $this->presion_arterial_paciente,
 
+			'Nivel_actividad' => $this->nivel_de_actividad,
 			'EER' => $this->valor_EER,
 			'Tasa_metabolica_basal' => $this->tasa_metabolica_basal,
 			'Gasto_energetico_total_estimado' => $this->gasto_energetico_total_estimado,
