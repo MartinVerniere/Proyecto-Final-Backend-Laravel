4c7cbee74f689cd7822ddddb5caed93abff7e703
MartinVerniere <mn.verniere@gmail.com>
2026-03-25 10:16:58 -0300

    Update PatientFactory.php

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -21,7 +21,7 @@ class PatientFactory extends Factory
             'nombre' => fake()->firstName(),
             'apellido' => fake()->lastName(), 
             'genero' => fake()->randomElement(['MASCULINO', 'FEMENINO']),
-            'DNI' => fake()->randomNumber(10000000, 100000000000000),
+            'DNI' => fake()->randomNumber(8),
             'fecha_nacimiento' => fake()->date(),
         ];
     }
