ca6dcdc93dabb7bfc74582bf081a98681ef74603
MartinVerniere <mn.verniere@gmail.com>
2026-04-12 10:01:30 -0300

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex flex-col gap-4 w-1/3 items-center justify-center p-5">
-	<img src="{{ asset('logos/logo.png') }}" class="w-full h-auto">
+	<img src="{{ url('logos/logo.png') }}" class="w-full h-auto">
 	<label class="font-bold text-black text-2xl">PostureSolver</label>
 </div>
